b84bbd0181cd3cefb26f72c6a63b7b8c046e3b0a
arkcheng <15843816+arkcheng@user.noreply.gitee.com>
2025-07-07 13:16:53 +0800

    第一次上传

---
+++ src/client/java/com/besson/tutorial/ArknightModClient.java
@@ -0,0 +1,10 @@
+package com.besson.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArknightModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
